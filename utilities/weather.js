dd68e1d67d9613f87864993963e604f04fa16be4
Jared Pines <pinesjared@gmail.com>
2022-11-15 12:37:38 -0800

    fixed weather

--- utilities/weather.js
+++ utilities/weather.js
@@ -3,54 +3,69 @@ const express = require('express')
 
 //retrieve the router object from express
 var router = express.Router()
-let callApi = (city) => {
-const fs = require('fs')
-if (fs.existsSync('weather.json')) {
-    return 'Weather data exists'
-}
-else{
-    fetch('https://api.weatherbit.io/v2.0/forecast/daily?city='+city+'&key=0262b79849e84a35866dc2600904ecd3').then(response =>{
-        return response.json();
-    }).then(data =>{
-        console.log(data);
-        fs.writeFile('weather.json', JSON.stringify(data), function (err) {
-        if (err) return console.log(err);
-            console.log('7dayforcast > weather.json');
-    });
-    })
-}
-    
-}
-let callApiGet = () => {
-    const fs = require('fs')
-    if (fs.existsSync('weather.json')) {
-        fs.readFile('weather.json', 'utf8', function (err,data) {
-            if (err) {
-              return console.log(err);
-            }
-            console.log('Weather data is stored');
-        });
-        return require('../weather.json')
-    }
-    else{
-        return "No weather stored."
-    }
-        
-    }
-    
+
+
 router.post("/", (request, response) => {
-    response.send({
-        //req.query is a reference to arguments a
-        message: callApi(request.body.city)
+    const city = request.body.city;
+    fetch('https://api.weatherbit.io/v2.0/forecast/daily?city='+city+'&key=0262b79849e84a35866dc2600904ecd3')
+        .then(response =>{
+            return response.json();
+        })
+        .then(data =>{
+
+            const payload = {
+                "days" : [
+                    {
+                        date: data.data[0].valid_date,
+                        max: data.data[0].max_temp,
+                        min: data.data[0].min_temp,
+                        description: data.data[0].weather.description
+                    },
+                    {
+                        date: data.data[1].valid_date,
+                        max: data.data[1].max_temp,
+                        min: data.data[1].min_temp,
+                        description: data.data[1].weather.description
+                    },
+                    {
+                        date: data.data[2].valid_date,
+                        max: data.data[2].max_temp,
+                        min: data.data[2].min_temp,
+                        description: data.data[2].weather.description
+                    },
+                    {
+                        date: data.data[3].valid_date,
+                        max: data.data[3].max_temp,
+                        min: data.data[3].min_temp,
+                        description: data.data[3].weather.description
+                    },
+                    {
+                        date: data.data[4].valid_date,
+                        max: data.data[4].max_temp,
+                        min: data.data[4].min_temp,
+                        description: data.data[4].weather.description
+                    },
+                    {
+                        date: data.data[5].valid_date,
+                        max: data.data[5].max_temp,
+                        min: data.data[5].min_temp,
+                        description: data.data[5].weather.description
+                    },
+                    {
+                        date: data.data[6].valid_date,
+                        max: data.data[6].max_temp,
+                        min: data.data[6].min_temp,
+                        description: data.data[6].weather.description
+                    }
+                ]
+            }
+            response.send({
+                //req.query is a reference to arguments a
+                message: payload
+            })
     })
 })
 
-router.get("/", (request, response) => {
-        response.send({
-            //req.query is a reference to arguments a
-            message: callApiGet()
-        })
-})
 
 
 
