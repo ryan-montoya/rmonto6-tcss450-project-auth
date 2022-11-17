597922206be93eda12f6adfd1011fd50086c5249
Jared Pines <pinesjared@gmail.com>
2022-11-17 12:36:57 -0800

    gives current api key

--- utilities/weather.js
+++ utilities/weather.js
@@ -3,7 +3,20 @@ const express = require('express')
 
 //retrieve the router object from express
 var router = express.Router()
-
+const api_keys = {
+    keys : [{
+        key1: "0262b79849e84a35866dc2600904ecd3"
+    },
+    {
+        key2: "188f548df07c4b31a4f9ba8663aaeb42"
+    },
+    {
+        key3: "09684b49b5344c0a87cd1961aa60b05c"
+    },
+    {
+        key4: "61a3155487884405a26387bc46edd87f"
+    }
+]}
 
 router.post("/", (request, response) => {
     const city = request.body.city;
@@ -12,7 +25,9 @@ router.post("/", (request, response) => {
         return response.json();
     })
     .then(data =>{
-    fetch('https://api.weatherbit.io/v2.0/forecast/daily?city='+city+'&key=' + data.message)
+        keys = JSON.stringify(api_keys.keys[data.message])
+        split = keys.split('\"')
+    fetch('https://api.weatherbit.io/v2.0/forecast/daily?city='+city+'&key=' + split[3])
         .then(response =>{
             return response.json();
         })
