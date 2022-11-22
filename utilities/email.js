9a8d17eab2a9d949ce86f1d253e2ac95e8d830d4
Jared Pines <pinesjared@gmail.com>
2022-11-22 08:43:49 -0800

    Testing email

--- utilities/email.js
+++ utilities/email.js
@@ -1,3 +1,8 @@
+//express is the framework we're going to use to handle requests
+const express = require('express')
+
+//retrieve the router object from express
+var router = express.Router()
 let sendEmail = (sender, receiver, subject, message) => {
     //research nodemailer for sending email from node.
     // https://nodemailer.com/about/
@@ -6,7 +11,31 @@ let sendEmail = (sender, receiver, subject, message) => {
     //make sure you add the password to the environmental variables
     //similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
 
-    //fake sending an email for now. Post a message to logs. 
+    //fake sending an email for now. Post a message to logs.
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'Smalltalktcss@gmail.com',
+    pass: 'P4a$$w0rd'
+  }
+});
+
+var mailOptions = {
+  from: sender,
+  to: receiver,
+  subject: subject,
+  text: message
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
     console.log("*********************************************************")
     console.log('To: ' + receiver)
     console.log('From: ' + sender)
@@ -16,7 +45,13 @@ let sendEmail = (sender, receiver, subject, message) => {
     console.log("*********************************************************")
 
 }
-
+router.post("/", (request, response) => {
+    sender = request.body.sender
+    receiver = request.body.receiver
+    subject = request.body.subject
+    message = request.body.message
+    sendEmail = (sender, receiver, subject, message)
+});
 module.exports = { 
-    sendEmail
+    router
 }
